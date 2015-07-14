743d7682d266abe0bccf6b777361cf9edff54af2
Isaac Priestley <progressions@gmail.com>
2015-07-14 14:42:18 -0500

    Annotate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  username        :string
+#  email           :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  location        :string
+#
+
 class User < ActiveRecord::Base
   has_secure_password
 

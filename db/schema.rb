aafef851f3c879697abae191d4d5449fa8dfa939
Isaac Priestley <progressions@gmail.com>
2015-07-14 11:27:34 -0500

    Add location to user, flash and nav stylesheets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713210612) do
+ActiveRecord::Schema.define(version: 20150714153333) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150713210612) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "location"
   end
 
 end
